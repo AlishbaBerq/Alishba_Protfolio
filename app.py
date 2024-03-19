bebedfa1c776be0bda3bdb39cb9cfeb69216e5b9
AlishbaBerq <163997877+AlishbaBerq@users.noreply.github.com>
2024-03-19 22:49:08 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Set the name of the Streamlit app
+app_name = "Alishba's Data Analysis Portfolio"
+
+# Configure the Streamlit page
+st.set_page_config(page_title=app_name, page_icon=":tada:", layout="wide")
+
+# Header Section
+with st.container():
+    st.subheader("HI, I am Alishba :wave:")
+    st.title("A Data Analyst from Pakistan")
+    st.write("I am passionate about finding ways to use Python and SQL to be more efficient and effective in business settings")
+    st.write("[Learn more>](https://www.facebook.com/profile.php?id=61557439354678&mibextid=ZbWKwL)")
