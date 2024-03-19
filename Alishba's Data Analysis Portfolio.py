b9df5cf7421936c056bfc06e598105fd9de25ce4
AlishbaBerq <163997877+AlishbaBerq@users.noreply.github.com>
2024-03-19 22:34:43 +0500

    Create Alishba's Data Analysis Portfolio.py

---
+++ Alishba's Data Analysis Portfolio.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Alishba's Data Analysis Portfolio", page_icon=":tada:", layout="wide")
+
+# ------ HEADER SECTION -------
+with st.container():
+    st.subheader("HI, I am Alishba :wave:")
+    st.title("A Data analyst from Pakistan")
+    st.write("I am passionate about finding ways to use Python and SQL to be more efficient and effective in business settings")
+    st.write("[Learn more>](https://www.facebook.com/profile.php?id=61557439354678&mibextid=ZbWKwL)")
